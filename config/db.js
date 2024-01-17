9818702db6dfffc60fb22a9df1365781dbd28f6f
Naushad Salmanee <87440062+naushad001@users.noreply.github.com>
2024-01-17 13:43:29 +0530

    final code
    Ecommerce App MERN

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import colors from "colors";
+
+const db= async () =>{  
+    try{
+        const conn = await mongoose.connect('mongodb+srv://naushad:naushad@cluster0.afq5pcw.mongodb.net/Ecommerce');
+        console.log(`conneted To MongoDB Database ${conn.connection.host}`.bgMagenta.white);
+    }
+ catch (error){
+        console.log(`Error in Mongodb ${error}`.bgRed.white)
+     }
+}
+
+export default db;
